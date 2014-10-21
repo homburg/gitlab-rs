d7e21f625a9f44e024f6636c2b911e149fd1a303
Thomas B Homburg <thomas@homburg.dk>
2014-10-21 20:22:09 +0200

    Project list

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ extern crate rest_client;
 extern crate serialize;
 
 use rest_client::RestClient;
+use std::os;
 use serialize::json;
 
 //    "description": null,
@@ -51,58 +52,29 @@ struct MergeRequest {
 	description: String,
 }
 
-    println!("{}", RestClient::get("").unwrap());
 
-    // You can use an array of tuples to create a GET with query parameters.
-    // The client handles all the URL-encoding and escaping for you.
+fn main() {
 
-    println!("{}", RestClient::get_with_params("http://example.com/resource", 
-                                               [("id", "50"), ("foo", "bar")]).unwrap());
+	let private_token = match os::getenv("GITLAB_PRIVATE_TOKEN") {
+		Some(token) => token,
+		None => fail!("You should specify a GITLAB_PRIVATE_TOKEN"),
+	};
 
-    // You can also use an array of tuples to create a POST with form parameters. 
-    // The client sets the content-type to application/x-www-form-urlencoded for you.
+	let gitlab_url = match os::getenv("GITLAB_URL") {
+		Some(url) => url,
+		None => fail!("You should specify a GITLAB_URL"),
+	};
 
-    println!("{}", RestClient::post_with_params("http://example.com/resource",
-                                                [("param1", "one"), 
-                                                 ("param2", "two")]).unwrap());
+    let projects_response = RestClient::get_with_params(
+		format!("{}/api/v3/projects", gitlab_url).as_slice(),
+		[("private_token", private_token.as_slice())]
+	).unwrap();
 
-    // You can POST a string or a JSON object with just a string and a MIME type.
-    println!("{}", RestClient::post("http://example.com/resource",
-                                    json::encode(&object).as_slice(), 
-                                    "application/json").unwrap());
+    // println!("{:d}", projects_response.code);
 
-    // PUT and PATCH are supported as well, just like POST.
+	let projects: Vec<Project> = json::decode(projects_response.body.as_slice()).unwrap();
 
-    // You can delete a resource with a simple DELETE. delete_with_params works too.
-
-    println!("{}", RestClient::delete("http://example.com/resource").unwrap());
-
-    /*
-      The response struct has a few fields
-      code (a simple integer)
-      body (a string)
-      status (a typed response code, from Hyper)
-      headers (typed headers from Hyper)
-    */
-
-    let response = RestClient::get("http://example.com/resource").unwrap();
-
-    println!("{:d}", response.code); // -> 404
-
-    for header in response.headers.iter() {
-        println!("{}", header); // -> (Cache-Control, max-age=604800) ...
-    }
-
-    println!("{}", response.to_string());                 
-
-    /*
-      All of the underlying errors are passed up through 
-      the RestError struct in the Result.
-
-      pub enum RestError {
-        UrlParseError(ParseError),
-        HttpRequestError(HttpError),
-        HttpIoError(IoError)
-      }
-    */
+	for project in projects.iter() {
+		println!("{name}", name=project.name);
+	}
 }
